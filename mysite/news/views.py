91bf6c8a1299e62a90bb4e1d6f01253dd890a6f8
Aleksei Gudov <80291332+scaps1@users.noreply.github.com>
2021-10-12 17:05:57 +0300

    заменил функции view на классы дженерики

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import News, Category
 from .forms import NewsForm
 
 
 class HomeNews(ListView):
     model = News
-    template_name = 'news/news_list.html' # название шаблона
-    context_object_name = 'news' # название объекта для работы вместо object_list
+    template_name = 'news/news_list.html'  # название шаблона
+    context_object_name = 'news'  # название объекта для работы вместо object_list
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -34,17 +33,11 @@ class NewsByCategory(ListView):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 
-def view_news(request, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+class AddNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
